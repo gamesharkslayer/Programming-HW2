c2e1e89bd786c7f03ba28c8a074031b70d06639c
charlie kaing <charlie6122@gmail.com>
2022-11-10 11:45:33 -0800

    Add project files.

---
+++ Programming HW2/Book.h
@@ -0,0 +1,34 @@
+class Book
+{
+    // complete the class
+
+private:
+    int code;
+    string title;
+    int available;
+    int rented;
+};
+
+class ChildrenBook
+    : public Book
+{
+    // complete the class
+private:
+    int age;
+};
+
+class ComputerBook
+    : public Book
+{
+    // complete the class
+private:
+    string publisher;
+};
+class Novel
+    : public Book
+{
+    // complete the class
+private:
+    int publish_date;
+};
+//Implmentation
---
+++ Programming HW2/Person.h
@@ -0,0 +1,25 @@
+// Person class
+class Person
+{
+    // complete the class
+private:
+    int id;
+    string name;
+    int count;
+};
+
+class Student
+    : protected Person
+{
+    // complete the class
+private:
+    int code[2];
+};
+
+class Teacher
+    : protected Person
+{
+    // complete the class
+private:
+    int code[3];
+};
