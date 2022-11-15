3dfc75f5d419da18ccf5764c7f1d47208d67ef23
charlie kaing <charlie6122@gmail.com>
2022-11-15 12:09:45 -0800

    Add implentation

--- Programming HW2/Book.h
+++ Programming HW2/Book.h
@@ -1,6 +1,19 @@
+using namespace std;
 class Book
 {
     // complete the class
+public:
+
+    Book();
+    Book(int code, string title, int available, int rented);
+    void setcode(int code);
+    int getcode();
+    void settitle(string title);
+    string gettitle();
+    void setavailable(int available);
+    int getavailable();
+    void setrented(int rented);
+    int getrented();
 
 private:
     int code;
@@ -9,26 +22,129 @@ private:
     int rented;
 };
 
-class ChildrenBook
-    : public Book
+class ChildrenBook : public Book
 {
     // complete the class
+public:
+    ChildrenBook() : Book(0, "Empty", 0, 0)
+    {
+        age = 0;
+    }
+    ChildrenBook(int code, string title, int available, int rented, int age) : Book(code, title, available, rented)
+    {
+        this->age = age;
+    }
+    void setage(int age);
+    int getage();
 private:
     int age;
 };
 
-class ComputerBook
-    : public Book
+class ComputerBook : public Book
 {
     // complete the class
+public:
+    ComputerBook() :Book(0, "Empty", 0, 0)
+    {
+        publisher = "Blank";
+    }
+    ComputerBook(int code, string title, int available, int rented, string publisher) : Book(code, title, available, rented)
+    {
+        this->publisher = publisher;
+    }
+    void setpublisher(string publisher);
+    string getpublisher();
 private:
     string publisher;
 };
-class Novel
-    : public Book
-{
-    // complete the class
+
+class Novel : public Book
+{ // complete the class
+public:
+    Novel() : Book(0, "Empty", 0, 0)
+    {
+        this->publish_date = 000000;
+    }
+    Novel(int code, string title, int available, int rented, int publish_date) : Book(code, title, available, rented)
+    {
+        this->publish_date = publish_date;
+    }
+    void setpublish(int date);
+    int getpublish();
 private:
     int publish_date;
 };
-//Implmentation
+
+struct BookNode
+{
+    Book book;
+    BookNode* link;
+};
+BookNode* library[3];
+
+Book::Book()
+{
+    this->code = 0000000;
+    this->title = "title";
+    this->available = 0;
+    this->rented = 0;
+}
+Book::Book(int code, string title, int available, int rented)
+{
+    this->code = code;
+    this->title = title;
+    this->available = available;
+    this->rented = rented;
+}
+void Book::setcode(int code)
+{
+    this->code = code;
+}
+int Book::getcode()
+{
+    return code;
+}
+void Book::settitle(string title)
+{
+    this->title = title;
+}
+string Book::gettitle()
+{
+    return title;
+}
+void Book::setavailable(int available)
+{
+    this->available = available;
+}
+void Book::setrented(int rented)
+{
+    this->rented = rented;
+}
+int Book::getrented()
+{
+    return rented;
+}
+void ChildrenBook::setage(int age)
+{
+    this->age = age;
+}
+int ChildrenBook::getage()
+{
+    return age;
+}
+void ComputerBook::setpublisher(string publisher)
+{
+    this->publisher = publisher;
+}
+string ComputerBook::getpublisher()
+{
+    return publisher;
+}
+void Novel::setpublish(int date)
+{
+    this->publish_date = date;
+}
+int Novel::getpublish()
+{
+    return publish_date;
+}
--- Programming HW2/Person.h
+++ Programming HW2/Person.h
@@ -1,25 +1,164 @@
+using namespace std;
 // Person class
 class Person
 {
     // complete the class
+public:
+    Person();
+    Person(int id, string name, int count);
+    int getid();
+    void setid(int id);
+    string getname();
+    void setname();
+    int getcount();
+    void setcount(int count);
 private:
     int id;
     string name;
     int count;
 };
 
-class Student
-    : protected Person
+class Student : protected Person
 {
     // complete the class
+public:
+    Student() : Person(0000, "Empty", 0)
+    {
+        code[0] = 0;
+        code[1] = 0;
+    }
+    Student(int id, string name, int count, int code1) : Person(id, name, count)
+    {
+        code[0] = code1;
+        code[1] = 0;
+    }
+    Student(int id, string name, int count, int code1,int code2) : Person(id,name,count)
+    {
+        code[0] = code1;
+        code[1] = code2;
+    }
+    void setcode1(int code);
+    void setcode2(int code);
+    void setcode3(int code);
+    int getcode1();
+    int getcode2();
 private:
     int code[2];
 };
 
-class Teacher
-    : protected Person
+class Teacher : protected Person
 {
     // complete the class
+public:
+    Teacher() : Person(0000, "Empty", 0)
+    {
+        code[0] = 0;
+        code[1] = 0;
+        code[2] = 0;
+    }
+    Teacher(int id, string name, int count, int code1) : Person(id, name, count)
+    {
+        code[0] = code1;
+        code[1] = 0;
+    }
+    Teacher(int id, string name, int count, int code1, int code2) : Person(id, name, count)
+    {
+        code[0] = code1;
+        code[1] = code2;
+        code[2] = 0;
+    }
+    Teacher(int id, string name, int count, int code1, int code2, int code3) : Person(id, name, count)
+    {
+        code[0] = code1;
+        code[1] = code2;
+        code[2] = code3;
+    }
+    void setcode1(int code);
+    void setcode2(int code);
+    void setcode3(int code);
+    int getcode1();
+    int getcode2();
+    int getcode3();
 private:
     int code[3];
 };
+
+Person::Person()
+{
+    id = 0000;
+    name = "Empty";
+    count = 0;
+}
+Person::Person(int id, string name, int count)
+{
+    this->id = id;
+    this->name = name;
+    this->count = count;
+}
+
+int Person::getid()
+{
+    return id;
+}
+void Person::setid(int id)
+{
+    this->id = id;
+}
+string Person::getname()
+{
+    return name;
+}
+void Person::setname()
+{
+    this->name = name;
+}
+int Person::getcount()
+{
+    return count;
+}
+void Person::setcount(int count)
+{
+    this->count = count;
+}
+
+void Student::setcode1(int code)
+{
+    this->code[0] = code;
+}
+void Student::setcode2(int code)
+{
+    this->code[1] = code;
+}
+int Student::getcode1()
+{
+    return code[0];
+}
+int Student::getcode2()
+{
+    return code[1];
+}
+
+void Teacher::setcode1(int code)
+{
+    this->code[0] = code;
+}
+void Teacher::setcode2(int code)
+{
+    this->code[1] = code;
+}
+void Teacher::setcode3(int code)
+{
+    this->code[2] = code;
+}
+int Teacher::getcode1()
+{
+    return code[0];
+}
+int Teacher::getcode2()
+{
+    return code[1];
+}
+int Teacher::getcode3()
+{
+    return code[2];
+}
